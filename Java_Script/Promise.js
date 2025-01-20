5692a82a861d2fcd0b6f4a988a366fd0fdfd9f97
Boobalank12345 <boobalan.k2023lcse@sece.ac.in>
2025-01-20 09:57:48 +0530

    day6 fn

--- Java_Script/Promise.js
+++ Java_Script/Promise.js
@@ -1,32 +1,63 @@
-// function promiseEg(flag){
-//     return new Promise(function(resolve,reject){
-// if (flag===1){
-//     resolve("This is a Promise Resolve Example")
-// }
-// else if(flag===0){
-//     reject("This is a Promise Reject Example")
-// }
-//     })
-// }
+/*function promiseEg(value){
+    /*var promise = new Promise(function(resolve,reject){
+        resolve("This is a Promise Example");
+    });
 
-import { time } from "console"
-
-
-
-
-
-
-
-
-// Promise - Location Finder Example
-// Pass 2 things - Melboune,2000
-
-locationFinder("Melboune",2000)
-locationFinder=(location,time)=>{
-    return new Promise((locFound,locNotFound)=>{
-        if(isLocationValid(location,time)){
-            locFound([location,"Found in",2000,"millsecs"])
-
+    return new Promise(function(resolve,reject){
+        if(value === 1){
+            resolve("This is a resolve Promise Example");
         }
+        else if(value === 0){
+            reject("This is a reject Promise Example")
+        }
+        
+    });
+
+}
+
+//console.log(promiseEg(7)); //Promise { <pending> }
+//console.log(promiseEg(0));
+promiseEg(0).then((res)=>{
+    console.log(res);
+}).catch((rej) =>{
+    console.log(rej);
+})
+*/
+
+//locationfinder
+
+//pass 2 things - melboune , 2000
+
+
+
+locationFinder = (location,time)=>{
+    return new Promise((locationFound,locationNotFound) =>{
+        setTimeout(()=>{
+            if(isLocationValid(location,time)){
+                locationFound([location,"Found in",time,"millisecs"]);
+            }
+            else{
+                locationNotFound([location,"Not Found in",time,"millisecs"]);
+            }
+        },time)
+        
     })
-}   
+    
+}
+
+function isLocationValid(loc,time){
+    location = "melboune";
+    t = 2000;
+    if(loc === location && time <= t){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+locationFinder("melboune",2001).then((result) =>{
+    console.log(result);
+}).catch((err) =>{
+    console.log(err);
+})
