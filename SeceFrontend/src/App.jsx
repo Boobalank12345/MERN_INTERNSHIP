b8c8b14a4342685e7f570bdcdfaea8d028a6b9ad
Boobalank12345 <boobalan.k2023lcse@sece.ac.in>
2025-01-21 09:03:51 +0530

    day7

--- SeceFrontend/src/App.jsx
+++ SeceFrontend/src/App.jsx
@@ -1,16 +1,18 @@
 import About from './components/about'
 import {ClassComponent,Gallery} from './components/classComp'
+import Contact from './components/Contact'
 import viteLogo from '/vite.svg'
 
 
 function App() {
   
   return (
-    <>
+    <div>
       < About />
       < ClassComponent />
       < Gallery image="React LOGO" page="picture"/>
-    </>
+      <Contact />
+    </div>
   )
 }
 
---
+++ SeceFrontend/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+const Contact = () =>{
+    var [num,setNum]=useState(0);
+    return (
+        <div>
+            <h2>
+                Welcome to conatct page
+            </h2>
+            <h3>
+                
+                This is State Example
+            </h3>
+            <h4>
+                Number is{num}
+            </h4>
+            <button onClick={()=>setNum(num+1)}>+</button>
+        </div>
+    );
+};
+export default Contact;
