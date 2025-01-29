8157e7d119229c971230e2060d6625af6308181c
Boobalank12345 <boobalan.k2023lcse@sece.ac.in>
2025-01-29 09:06:52 +0530

    Day14 FN

--- SeceFrontend/src/components/Signup.jsx
+++ SeceFrontend/src/components/Signup.jsx
@@ -1,44 +1,83 @@
-import { Link } from "react-router-dom"
-import "./../css/signUp.css"
-import { useState } from "react"
-const Signup=()=>{
-    var[firstname,setFirstname]=useState("");
-    var[lastname,setlastname]=useState("");
-    var[username,setusername]=useState("");
-    var[email,setemail]=useState("");
-    var[password,setpassword]=useState("");
-    const handleSignup=()=>{
-        console.log("Event Triggered")
-    }
-    return (
-    
-        <div className="signup">
-            <form>
-                <div className="input">
-                    <label htmlFor="FirstName">FirstName</label>
-                    <input type="text" name="name" placeholder="enter your name"/>
-                </div>
-                <div className="input">
-                    <label htmlFor="LastName">LastName</label>
-                    <input type="text" name="name" placeholder="enter your name"/>
-                </div>
-                <div className="input">
-                    <label htmlFor="UserName">UserName</label>
-                    <input type="text" name="name" placeholder="enter your name"/>
-                </div>
-                    <div className="input">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" placeholder="enter your email" />
-                </div>
-                <div className="input">
-                    <label htmlFor="password">Password</label>
-                    <input type="text" name="password" placeholder="enter your password"/>
-                    
-                </div>
-                <Link to='/home' className="button">Signup</Link>
-            </form>
+import React, { useState } from 'react';
+import "../../css/Signup.css";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
+  return (
+    <div className="body">
+      <div className="center">
+        <div className="text">
+          {isLogin ? 'Login Form' : 'Signup Form'}
         </div>
-        
-    )
-}
-export default Signup;
+        <form action="#">
+          {isLogin ? (
+            <>
+              <div className="data">
+                <label>Email or Phone</label>
+                <input type="text" required />
+              </div>
+              <div className="data">
+                <label>Password</label>
+                <input type="password" required />
+              </div>
+              <div className="forgot-pass">
+                <a href="#">Forgot Password?</a>
+              </div>
+              <div className="btn">
+                  <div className="inner"></div>
+                  <Link to="/home"><button className="log-but" type="submit">login</button></Link>
+               </div>
+
+            </>
+          ) : (
+            <>
+              <div className="data">
+                <label>First Name</label>
+                <input type="text" required />
+              </div>
+              <div className="data">
+                <label>Last Name</label>
+                <input type="text" required />
+              </div>
+              <div className="data">
+                  <label>Username</label>
+                  <input type="text" required />
+               </div>
+              <div className="data">
+                <label>Email</label>
+                <input type="email" required />
+              </div>
+              <div className="data">
+                <label>Password</label>
+                <input type="password" required />
+              </div>
+              <div className="btn">
+                  <div className="inner"></div>
+                  <Link to="/home"><button className="log-but" type="submit">login</button></Link>
+               </div>
+            </>
+          )}
+          <div className="signup-link">
+            {isLogin ? (
+              <>
+                Not a member? <a href="#" onClick={toggleForm}>Signup now</a>
+              </>
+            ) : (
+              <>
+                Already a member? <a href="#" onClick={toggleForm}>Login now</a>
+              </>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
